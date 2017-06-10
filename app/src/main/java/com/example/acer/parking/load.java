25018965ac73d58d437b63acac423de9c28c0a64
WangXinTang <1403996861@qq.com>
2017-06-10 14:41:02 +0800

    restore

--- app/src/main/java/com/example/acer/parking/load.java
+++ app/src/main/java/com/example/acer/parking/load.java
@@ -50,7 +50,6 @@ public class load extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        System.out.flush();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load);
         mAccount = (EditText) findViewById(R.id.edit_user_id);
