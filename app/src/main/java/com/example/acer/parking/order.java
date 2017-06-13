2453580df6829775eb2332ae5ff601c09b35ca15
WangXinTang <1403996861@qq.com>
2017-06-13 21:54:14 +0800

    提交

--- app/src/main/java/com/example/acer/parking/order.java
+++ app/src/main/java/com/example/acer/parking/order.java
@@ -22,7 +22,16 @@ public class order extends AppCompatActivity {
         Intent intent_to_renting_out=new Intent(order.this,renting_out_order.class);
         startActivity(intent_to_renting_out);
     }
-
+    void doPost_creating_offer()
+    {
+        Intent intent_order_to_creating_offer=new Intent(order.this,creating_offer.class);
+        startActivity(intent_order_to_creating_offer);
+    }
+    void doPost_creating_proposal()
+    {
+        Intent intent_order_to_creating_proposal=new Intent(order.this,creating_offer.class);
+        startActivity(intent_order_to_creating_proposal);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +39,17 @@ public class order extends AppCompatActivity {
 
         renting_in_button=(Button) findViewById(R.id.renting_in_order);
         renting_out_button=(Button) findViewById(R.id.renting_out_order);
+        creating_offer_button=(Button) findViewById(R.id.create_offer);
+        creating_proposal_button=(Button) findViewById(R.id.create_proposal);
         renting_in_button.setOnClickListener(view->doPost_renting_in());
+
         renting_out_button.setOnClickListener(view->doPost_renting_out());
+
+        creating_offer_button.setOnClickListener(view->doPost_creating_offer());
+
+        creating_proposal_button.setOnClickListener(view->doPost_creating_proposal());
+
+
 
     }
 }
